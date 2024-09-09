57525aeeef80f335bb2bf7b9f2c7e4f1a87897d4
Juan Daniel Acosta Saavedra <71455647+heliosburning@users.noreply.github.com>
2024-09-09 10:45:26 -0600

    Update index.php

--- index.php
+++ index.php
@@ -15,19 +15,7 @@
   } else {
     echo "Conexión exitosa a PostgreSQL.\n";
   }
-
-  // Consulta SQL
-  $query = 'SELECT * FROM tabla_ejemplo'; // Asegúrate de cambiar 'tabla_ejemplo' por el nombre real de tu tabla
-  $result = pg_query($connection, $query);
-
-  // Mostrar resultados
-  if ($result) {
-    while ($row = pg_fetch_assoc($result)) {
-      echo "ID: " . $row['id'] . ", Nombre: " . $row['nombre'] . "\n"; // Cambia 'id' y 'nombre' por los nombres de las columnas de tu tabla
-    }
-  } else {
-    echo "Error en la consulta: " . pg_last_error($connection);
-  }
+echo "Sombreros de paja";
 
   // Cerrar conexión
   pg_close($connection);
